b4168ff90c25fb5f7881fea53a9816023a31ab82
HankTheTank <rjoydeb622@gmail.com>
2024-09-09 10:27:31 +0530

    config file changed

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABJbTMHKy_XpgWFjCFzPf2l6TfpGhTkCE",
-  authDomain: "cb-story-app-27012024.firebaseapp.com",
-  projectId: "cb-story-app-27012024",
-  storageBucket: "cb-story-app-27012024.appspot.com",
-  messagingSenderId: "26759520523",
-  appId: "1:26759520523:web:a0e7d0f73870a266ac420e",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
